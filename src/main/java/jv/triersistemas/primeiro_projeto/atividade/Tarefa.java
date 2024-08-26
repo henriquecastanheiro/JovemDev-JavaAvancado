7f45975eb574964f07e2aaf5bfa1b07c9b43de74
Henrique Castanheiro <henrique.castanheirotb@gmail.com>
2024-08-26 17:10:01 -0300

    Atividade01

--- src/main/java/jv/triersistemas/primeiro_projeto/atividade/Tarefa.java
+++ src/main/java/jv/triersistemas/primeiro_projeto/atividade/Tarefa.java
@@ -3,10 +3,12 @@ package jv.triersistemas.primeiro_projeto.atividade;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class Tarefa {
 	
 	private Long id;
--- src/main/java/jv/triersistemas/primeiro_projeto/controller/atividade/TarefaController.java
+++ src/main/java/jv/triersistemas/primeiro_projeto/controller/atividade/TarefaController.java
@@ -2,12 +2,12 @@ package jv.triersistemas.primeiro_projeto.controller.atividade;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ import jv.triersistemas.primeiro_projeto.atividade.Tarefa;
 public class TarefaController {
 	
 	public static List<Tarefa> tarefas = new ArrayList<Tarefa>();
-	public static Long contador;
+	public static Long contador = 0L;
 	
 	@GetMapping("/tarefas")
 	public List<Tarefa> getAll(){
@@ -28,14 +28,53 @@ public class TarefaController {
 	
 	@PostMapping()
 	public void cadastraTarefa(@RequestBody Tarefa tarefa) {
+		contador++;
+		tarefa.setId(contador);
 		tarefas.add(tarefa);
 	}
 	
-	@GetMapping("/tarefas/{contador}")
-	public static ResponseEntity<Stream<Tarefa>> finbById(@PathVariable Long contador){
-		return ResponseEntity.ok().body(tarefas.stream().filter(s -> s.getId()==contador));
+	@PostMapping("/tarefas/lote")
+	public void cadastraTarefasEmLote(@RequestBody List<Tarefa> novasTarefas) {
+	    novasTarefas.forEach(tarefa -> {
+	        contador++;
+	        tarefa.setId(contador);
+	        tarefas.add(tarefa);});
 	}
+
+	
+	@GetMapping("/tarefas/{id}")
+	public Tarefa finbById(@PathVariable Long id){
+		return tarefas.stream()
+				.filter(s -> s.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+	}
+	
+	@PutMapping("/tarefas/{id}")
+	public Tarefa atualizarTarefa(@PathVariable Long id, @RequestBody Tarefa tarefaAtualizada){
+		Tarefa tarefaEscolhida = tarefas.stream().
+				filter(s -> s.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+		
+		tarefaEscolhida.setTitulo(tarefaAtualizada.getTitulo());
+        tarefaEscolhida.setDescricao(tarefaAtualizada.getDescricao());
+        tarefaEscolhida.setCompleta(tarefaAtualizada.isCompleta());
+
+        return tarefaEscolhida;
+    }
+	
+	@DeleteMapping("/tarefas/{id}")
+    public void removerTarefa(@PathVariable Long id) {
+        Tarefa tarefaEscolhida = tarefas.stream()
+        		.filter(tarefa -> tarefa.getId().equals(id))
+        		.findFirst()
+        		.orElse(null);
+       
+        tarefas.remove(tarefaEscolhida);
+    }
 }
+	
 	
 	
 
