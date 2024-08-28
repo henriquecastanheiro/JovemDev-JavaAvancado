a070a0ebcbde3c8984e1fd2409200039b0940ac9
Henrique Castanheiro <henrique.castanheirotb@gmail.com>
2024-08-28 14:45:27 -0300

    ...

--- src/main/java/desafio_dados/controller/DadosController.java
+++ src/main/java/desafio_dados/controller/DadosController.java
@@ -2,7 +2,7 @@ package desafio_dados.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import desafio_dados.service.DadosService;
 
 @RestController
-@RequestMapping("/verificaDados")
+@RequestMapping("/aposta")
 public class DadosController {
 
     @Autowired
     private DadosService dadosService;
 
-    @GetMapping("/verificar")
+    @PostMapping("/jogarDados")
     public ResponseEntity<String> verificarDados(
-            @RequestParam Integer qtdDados,
+            @RequestParam Integer qtddDados,
             @RequestParam Integer aposta) {
-        return dadosService.verificarDados(qtdDados, aposta);
+        return dadosService.verificarDados(qtddDados, aposta);
     }
 }
